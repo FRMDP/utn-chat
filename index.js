24fb2c05afc2ffc22bbee07003a13c82c4aa0e10
Lisandro Cooper <coopermegadeth@gmail.com>
2017-10-25 23:44:53 -0300

    ahi esta

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let express = require('express');
+
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
@@ -7,9 +9,14 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.use(express.static(__dirname + '/css'));
+
 io.on('connection', (socket) => {
-	socket.on('chat.message', (msg) => {
-		io.emit('chat.message', msg);
+	socket.on('chat.message', (payload) => {
+		io.emit('chat.message', {
+      username: payload.userNameP,
+      msj: payload.newMessageP
+    });
 	});
 });
 
