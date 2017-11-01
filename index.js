8dc86083b9acbd6900819d91e0c15a7490846bfc
Lisandro Cooper <coopermegadeth@gmail.com>
2017-11-01 16:49:30 -0300

    ahi esta compa

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ let express = require('express');
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 3002;
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -13,10 +12,7 @@ app.use(express.static(__dirname + '/css'));
 
 io.on('connection', (socket) => {
 	socket.on('chat.message', (payload) => {
-		io.emit('chat.message', {
-      username: payload.userNameP,
-      msj: payload.newMessageP
-    });
+		io.emit('chat.message', payload);
 	});
 });
 
